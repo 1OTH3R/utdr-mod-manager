a89a0f326713c02ebead30dec779d15c57fb4e99
1OTH3R <colton@mcadoo.com>
2026-06-16 17:28:12 -0500

    Removed everything to prep for v1.1 update!

--- start_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Killing process using port 8000...
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8000 ^| findstr LISTENING') do (
-    echo Killing PID %%a
-    taskkill /PID %%a /F
-)
-timeout /t 1 /nobreak >nul
